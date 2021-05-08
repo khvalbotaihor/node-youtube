0ea6cb601d0bf522cdc0723a7750f573cc560e99
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-08 15:50:52 +0300

    - update

--- demo/fs.js
+++ demo/fs.js
@@ -2,6 +2,9 @@
 const fs = require('fs')
 const path = require('path')
 
-fs.mkdir(path.join(__dirname, 'test', () => {
-
+fs.mkdir(path.join(__dirname, 'test', (err) => {
+    if (err) {
+        throw err
+    }
+    console.log('The directory is created')
 }))
