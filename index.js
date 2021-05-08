d8b64d12f28fcf284912507df4c5a62555cca3c3
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-08 15:30:20 +0300

    - update

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-(function (exports, require, mpdule, _dirname, _filename) {
-        const chalk = require('chalk');
-        const text = require('./data')
+//(function (exports, require, mpdule, _dirname, _filename) {
+const chalk = require('chalk');
+const text = require('./data')
 
 
-        console.log(chalk.blue(text))
-    }
-)
+console.log(chalk.blue(text))
+console.log(__dirname)
+console.log(__filename)
+//    })
