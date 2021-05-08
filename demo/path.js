537889da7cf8f4dbac07aa052f99969f978cd27c
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-08 15:37:57 +0300

    - update

--- demo/path.js
+++ demo/path.js
@@ -2,3 +2,4 @@ const path = require('path')
 
 console.log('Filename: ', path.basename(__filename))
 console.log('Directory name: ', path.dirname(__filename))
+console.log('File extension: ', path.extname(__filename))
