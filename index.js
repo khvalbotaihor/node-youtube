49b7da911eea578b9a2fcef2f1e8af64c75ee8b0
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-09 17:51:54 +0300

    - added new files

--- index.js
+++ index.js
@@ -10,21 +10,21 @@ const http = require('http')
 const fs = require('fs')
 const path = require('path')
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-type': 'text/html'
-    })
 
-    if (req.url === '/'){
+    if (req.url === '/') {
         fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
-        if (err){
-            throw err
-        }
+            if (err) {
+                throw err
+            }
+            res.writeHead(200, {
+                'Content-type': 'text/html'
+            })
+            res.end('<h1>Hello NodeJS!!!!</h1>')
         })
     }
 
-    res.end('<h1>Hello NodeJS!!!!</h1>')
 })
 
-server.listen(3000, () =>{
+server.listen(3000, () => {
     console.log('Server has been started...')
 })
