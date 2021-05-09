985c6aedb184b0e6b9df824a8d3b7f8651cb3391
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-09 17:47:10 +0300

    - added new files

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {
         'Content-type': 'text/html'
     })
+    console.log(req.uri)
     res.end('<h1>Hello NodeJS!!!!</h1>')
 })
 
