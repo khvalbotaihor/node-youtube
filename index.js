be93011866897b979893e665e74787d7d534b856
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-12 07:51:00 +0300

    - added new files

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ const server = http.createServer((req, res) => {
     }*/
 let filePath = path.join(__dirname, 'public', req.url==='/' ? 'index.html' : req.url)
 const ext = path.extname(filePath)
+
+    if (!ext){
+        filePath += '.html' +
+            ''
+    }
+
     console.log(filePath)
     fs.readFile(filePath, (err, content) => {
         if (err){
